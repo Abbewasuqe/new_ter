7c9a788c27389868c9daba38505e382a91805233
abbewasuqe <abdullah.wasuqe@chasacademy.se>
2023-03-07 11:19:23 +0100

    run

--- main.tf
+++ main.tf
@@ -4,42 +4,42 @@ provider "github" {
 terraform {
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "5.16.0"
     }
   }
 }
 
 module "repositories" {
-  source = "./repositories"
-  default_topics = [ "ksad", "asdasd" ]
-  default_autolink_references = [ {
-    is_alphanumeric = false
-    key_prefix = "TICKET-"
+  source         = "./repositories"
+  default_topics = ["ksad", "asdasd"]
+  default_autolink_references = [{
+    is_alphanumeric     = false
+    key_prefix          = "TICKET-"
     target_url_template = "https://example.com/TICKET?query=<num>"
-  } ]
+  }]
   default_branch_protections = {
     "release-*" = {
       required_linear_history = false
     }
   }
   repository = [{
-    name = "my-repo"
-    description = "my new repo"
-      default_branch_name = "main"
+    name                   = "my-repo"
+    description            = "my new repo"
+    default_branch_name    = "main"
     delete_branch_on_merge = true
-  },
-  {
-    name = "enzo"
-    description = "second repo"
-      default_branch_name = "master"
+    },
+    {
+      name                   = "enzo"
+      description            = "second repo"
+      default_branch_name    = "master"
       delete_branch_on_merge = false
-  },
-  {
-    name = "test"
-    description = "test"
-    default_branch_name = "main"
+    },
+    {
+      name                   = "test"
+      description            = "test"
+      default_branch_name    = "main"
       delete_branch_on_merge = true
-  }
+    }
   ]
 }
